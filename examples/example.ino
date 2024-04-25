595b709df83f04e346726ba7bdeb992b97f98c9f
Arnab Mallick <35038282+Arnab-m1@users.noreply.github.com>
2024-04-25 17:36:57 +0530

    changed

--- examples/example.ino
+++ examples/example.ino
@@ -1,18 +1,19 @@
 #include <SecureMQTTClient.h>
 
+// Configure your network and MQTT settings
 SecureMQTTClient client(
-    "yourSSID", "yourWifiPassword", "mqtt.example.com", 8883, 
-    "user", "password", "/cert.der", "/private.der", "/ca.der", 
+    "YourWiFiSSID", "YourWifiPassword", "mqtt.example.com", 8883,
+    "mqttUser", "mqttPassword", "/cert.der", "/private.der", "/ca.der",
     "topic/test"
 );
 
 void setup() {
     Serial.begin(9600);
-    client.begin();
+    client.begin(); // Initialize and connect
 }
 
 void loop() {
-    client.loop();
+    client.loop(); // Maintain connectivity and handle MQTT
     client.publish("Hello from ESP8266 with secure MQTT!");
     delay(2000); // Publish every 2 seconds
 }
